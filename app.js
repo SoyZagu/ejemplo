6493f9eae87c4ec4167cf49ab8d210881bc69bc5
czamora <lx08396@innovaccion.mx>
2022-11-10 21:53:48 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,19 @@
+function moverPosicionRandom(elm){
+    elm.style.position = 'absolute';
+    elm.style.top = Math.random() * (window.innerHeight - elm.offsetHeight) + 'px';
+    elm.style.left = Math.random() * (window.innerWidth - elm.offsetWidht) + 'px';
+}
+
+let btn_si = document.getElementById("btn_si");
+let btn_no = document.getElementById("btn_no");
+let divModoSexo = document.getElementsByClassName("modo_sexo")[0];
+
+btn_no.addEventListener('mouseenter', function(e) {moverPosicionRandom(e.target)})
+
+btn_si.addEventListener('click', function(e) {
+    alert("Sabía que dirias que SI. Casemonos ya ❤")
+
+    divModoSexo.style.display = 'block';
+    const cancion = new Audio('img\\Parzica.mp3');
+    cancion.play();
+})
